10b06a7ce7ad97be349458dddeeac2a0a888be90
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2024-02-26 17:51:26 +0100

    test

--- lua/test_plugin/init.lua
+++ lua/test_plugin/init.lua
@@ -6,6 +6,10 @@ function setup(user_config)
 	vim.tbl_deep_extend("force", opts, user_config)
 end
 
+function config(user_config)
+	vim.tbl_deep_extend("force", opts, user_config)
+end
+
 function test()
 	if opts.what == 0 then
 		vim.cmd("echo 'pingpang'")
